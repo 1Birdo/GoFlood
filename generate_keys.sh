fd27d7064fe4ac9797a24fb3cd1f5ca86f8d4dfc
Birdo <81320346+1Birdo@users.noreply.github.com>
2025-06-27 20:37:08 +0100

    Delete generate_keys.sh

--- generate_keys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hex_key=$(head -c 32 /dev/urandom | xxd -p)
-base64_key=$(head -c 32 /dev/urandom | base64)
-echo -n "32-byte Random Key (Hex): "
-echo "$hex_key"
-echo -n "32-byte Random Key (Base64): "
-echo "$base64_key"
